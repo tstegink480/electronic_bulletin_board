e20b2a99b7703431eb7d6e47c12c4625e30a0910
Jonathan Geisler <jgeisler@cse.taylor.edu>
2012-11-14 16:53:05 -0500

    Use FactoryGirl for testing
    This commit uses FactoryGirl to create models for testing.

--- spec/models/advertisement_spec.rb
+++ spec/models/advertisement_spec.rb
@@ -1,15 +1,33 @@
 require 'spec_helper'
 
 describe Advertisement do
-  before { @ad = Advertisement.new }
+  let(:user) { FactoryGirl.create(:user) }
+  let(:board) { FactoryGirl.create(:board) }
+  let(:ad) { FactoryGirl.create(:advertisement, user: user, board: board) }
 
-  subject { @ad }
+  subject { ad }
 
   it { should respond_to(:x_location) }
   it { should respond_to(:y_location) }
   it { should respond_to(:height) }
   it { should respond_to(:width) }
+  it { should respond_to(:user_id) }
   it { should respond_to(:user) }
   it { should respond_to(:board) }
+  it { should respond_to(:board_id) }
   it { should respond_to(:image) }
+
+  describe 'accessible attributes' do
+    it 'should not allow access to user_id' do
+      expect do
+	Advertisement.new(user_id: user)
+      end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
+
+    it 'should not allow access to board_id' do
+      expect do
+	Advertisement.new(board_id: board)
+      end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
+  end
 end
--- spec/models/tile_spec.rb
+++ spec/models/tile_spec.rb
@@ -1,13 +1,32 @@
 require 'spec_helper'
 
 describe Tile do
-  before { @tile = Tile.new }
+  let(:user) { FactoryGirl.create(:user) }
+  let(:board) { FactoryGirl.create(:board, user: user) }
+  let(:ad) { FactoryGirl.create(:advertisement, user: user, board: board) }
+  let(:tile) { FactoryGirl.create(:tile, board: board, advertisement: ad) }
 
-  subject { @tile }
+  subject { tile }
 
   it { should respond_to(:board) }
+  it { should respond_to(:board_id) }
   it { should respond_to(:advertisement) }
+  it { should respond_to(:advertisement_id) }
   it { should respond_to(:x_location) }
   it { should respond_to(:y_location) }
   it { should respond_to(:cost) }
+
+  describe 'accessible attributes' do
+    it 'should not allow access to board_id' do
+      expect do
+	Tile.new(board_id: board)
+      end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
+
+    it 'should not allow access to advertisement_id' do
+      expect do
+	Tile.new(advertisement_id: ad)
+      end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
+  end
 end
