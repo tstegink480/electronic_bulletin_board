731c6eb459b965946a2fbca95ae57d3860ecc576
Jonathan Geisler <jgeisler@cse.taylor.edu>
2012-11-13 21:31:05 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElectronicBulletinBoard::Application.config.secret_token = '0bc543f320226914e59e1d6456d16b357be425c89568169e4365584d8fa01c137dcdd18280b6788c16c704d021691064ae9b85136f22a4a46468b7fb6f2e7eef'
