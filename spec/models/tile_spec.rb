1077853bb07b57baed99aaecdfdf5a3833b40218
Jonathan Geisler <jgeisler@cse.taylor.edu>
2012-11-15 22:59:00 -0500

    Validate tile attributes.
    These tests ensure that the tile model has the right values at all
    times.

--- spec/models/tile_spec.rb
+++ spec/models/tile_spec.rb
@@ -28,5 +28,105 @@ describe Tile do
 	Tile.new(advertisement_id: ad)
       end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
     end
+
+    it 'should not allow access to cost' do
+      expect do
+	Tile.new(cost: 1)
+      end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
+  end
+
+  #
+  # It is important to note that because FactoryGirl creates the
+  # objects in the DB, it expects them to validate successfully before
+  # returning.  This is important with let since we are lazily
+  # creating our objects, so the first time we see an object it is
+  # also being created for us.  We need to be sure that the order we
+  # list the objects will ensure they remain valid upon creation.  We
+  # could solve this by using build instead of create, but I expect
+  # we'll want truly created objects for future tests.  In the
+  # meantime, if we put tile before manipulating board, ad, or user,
+  # then things should "just work".
+  #
+  describe 'validating attributes' do
+    describe 'x_location' do
+      describe 'negative' do
+	before { tile.x_location = -1 }
+
+	it { should_not be_valid }
+      end
+
+      describe 'larger than board width' do
+	before do
+	  tile.x_location = 5
+	  board.width = 5
+	end
+
+	it { should_not be_valid }
+      end
+
+      describe 'smaller than ad x_location' do
+	before do
+	  tile.x_location = 7
+	  ad.x_location = 9
+	end
+
+	it { should_not be_valid }
+      end
+
+      describe 'past end of ad width' do
+	before do
+	  tile.x_location = 4
+	  ad.x_location = 0
+	  ad.width = 4
+	end
+
+	it { should_not be_valid }
+      end
+    end
+
+    describe 'y_location' do
+      describe 'negative' do
+	before { tile.y_location = -1 }
+
+	it { should_not be_valid }
+      end
+
+      describe 'larger than board height' do
+	before do
+	  tile.y_location = 5
+	  board.height = 5
+	end
+
+	it { should_not be_valid }
+      end
+
+      describe 'smaller than ad y_location' do
+	before do
+	  tile.y_location = 7
+	  ad.y_location = 9
+	end
+
+	it { should_not be_valid }
+      end
+
+      describe 'past end of ad height' do
+	before do
+	  tile.y_location = 4
+	  ad.y_location = 0
+	  ad.height = 4
+	end
+
+	it { should_not be_valid }
+      end
+    end
+
+    describe 'cost' do
+      describe 'negative' do
+	before { tile.cost = -1 }
+
+	it { should_not be_valid }
+      end
+    end
   end
 end
