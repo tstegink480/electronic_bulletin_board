87aa9920c5f8cce6a9755bd44ca124b595ac42a3
Jonathan Geisler <jgeisler@cse.taylor.edu>
2012-11-28 00:42:14 -0500

    Initial tests for aging advertisements.
    These tests primarily test for being able to age advertisements.  They
    require that the models support a new method (age for Board and Tile
    and charge for Advertisement) so that the aging can be accomplished
    properly.  The idea is that some other process (e.g., a cron job)
    could run and call the age method on the proper models to cause them
    to reset values and create a new charge for balance purposes.
    
    More tests to follow...

--- spec/requests/advertisement_pages_spec.rb
+++ spec/requests/advertisement_pages_spec.rb
@@ -45,12 +45,12 @@ describe "AdvertisementPages" do
       it 'should create width x height tiles' do
 	expect do
 	  click_button submit
-	end.to change(Tile, :count).by(15)
+	end.to change { Tile.last.id }.by(15)
       end
 
       describe 'after saving the advertisment' do
 	let(:ad) { Advertisement.last }
-	let(:payment) { ad.payment_details.first }
+	let(:payment) { ad.payment_details.last }
 
 	before { click_button submit }
 
@@ -66,6 +66,23 @@ describe "AdvertisementPages" do
 	it { should have_content(ad.user.name) }
 	it { should have_selector("img#ad_#{ad.id}_image") }
       end
+    end
+  end
+
+  describe 'having an advertisement age' do
+    let(:ad) { FactoryGirl.create(:advertisement, height: 4, width: 4) }
+    let(:payment) { ad.payment_details.last }
+
+    describe 'should reduce in cost by half' do
+      before { ad.board.age }
+
+      specify { payment.amount.should == 4 * 4 / 2 }
+    end
+
+    describe 'should never cost less than $0.01' do
+      before { 7.times { ad.board.age } }
+
+      specify { payment.amount.should == 0 }
     end
   end
 end
