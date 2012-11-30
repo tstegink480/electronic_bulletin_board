3941525b33afd3472c2068cdea875163133902b0
Trent Stegink <trent_stegink@taylor.edu>
2012-11-29 21:28:43 -0500

    11/29 65 Failures

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,67 @@
 class SessionsController < ApplicationController
-		def new
-  	end
 
-	  def create
-		end
+  def new
+  end
 
-		def destroy
-		end
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+        session[:user] = user.id
+        sign_in user
+        redirect_back_or user
+    else
+        flash.now[:error] = 'Invalid email/password combination'
+        render 'new'
+    end 
+  end
+
+def destroy
+    sign_out
+    redirect_to root_path
+end
+
+
+  def sign_in(user)
+    cookies.permanent[:remember_token] = user.remember_token
+    self.current_user = user
+  end
+  
+  def signed_in?
+    !current_user.nil?
+  end
+  
+  def current_user=(user)
+    @current_user = user
+  end
+  
+  def current_user
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+  end
+  
+  def current_user?(user)
+    user == current_user
+  end
+  
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+  
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url
+  end
+
+
 end
